a60cd3dcfac76035165c6a49f9920e7714fc9757
Nathan Araújo <nathanaraujoerocha@gmail.com>
2023-07-03 02:00:36 -0300

    whatsapp and mega_bazar logic to main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from gooey import Gooey, GooeyParser, local_resource_path
 import signal
-from utils import save_config, load_config
+from utils import save_config, load_config, save_cache, load_cache
+from mega_bazar import valid_contacts
+from whatsapp import WhatsApp
 
 
 @Gooey(
@@ -29,20 +31,77 @@ def main():
 
     parser = GooeyParser(description="Vamos enviar sua mensagem ( ＾◡＾ )")
     parser.add_argument(
-        "ArquivoContatos", widget="FileChooser", default=config["ArquivoContatos"]
+        "ArquivoContatos",
+        widget="FileChooser",
+        default=config["ArquivoContatos"],
+        help="Arquivo CSV com os contatos do Google",
     )
-    parser.add_argument("Imagem", widget="FileChooser", default=config["Imagem"])
+    parser.add_argument(
+        "Imagem",
+        widget="FileChooser",
+        default=config["Imagem"],
+        help="A imagem que você deseja enviar",
+    )
     # TODO workaround for emoji in the end getting lost
-    parser.add_argument("Texto", widget="Textarea", default=config["Texto"])
+    parser.add_argument(
+        "Texto",
+        widget="Textarea",
+        default=config["Texto"],
+        help="O texto que você deseja enviar como descrição da imagem",
+        gooey_options={"full_width": True},
+    )
+    parser.add_argument(
+        "Continuação",
+        choices=["Sim", "Não"],
+        default="Não",
+        help="Marque esse campo se deseja continuar de onde parou a última execução",
+    )
+    parser.add_argument(
+        "TentarErrados",
+        choices=["Sim", "Não"],
+        default="Não",
+        help="Marque esse campo se deseja tentar os números que já falharam alguma vez",
+    )
     args = parser.parse_args()
-    
+
     config["ArquivoContatos"] = args.ArquivoContatos
     config["Imagem"] = args.Imagem
     config["Texto"] = args.Texto
-    
+
     save_config(config)
 
-    print(args.Texto)
+    contacts = valid_contacts(args.ArquivoContatos)
+
+    wpp = WhatsApp()
+    
+    wpp.start()
+
+    wpp.set_image_data(args.Imagem)
+
+    sent_contact_names = load_cache("sent_contact_names", set())
+    continuation = args.Continuação == "Sim"
+
+    failed_contact_phones = load_cache("failed_contact_phones", set())
+    try_wrong = args.TentarErrados == "Sim"
+
+    for contact in contacts:
+        name = contact["name"]
+        if not continuation or name not in sent_contact_names:
+            for phone in contact["phones"]:
+                if try_wrong or phone not in failed_contact_phones:
+                    print(f"Enviando para '{name}' ({phone})")
+                    try:
+                        wpp.do_all_send_image(name, phone, args.Texto)
+                        sent_contact_names.add(name)
+                        save_cache("sent_contact_names", sent_contact_names)
+                    except Exception as e:
+                        failed_contact_phones.add(phone)
+                        save_cache("failed_contact_phones", failed_contact_phones)
+                        print(f"Não foi possível enviar a mensagem para '{name}' através do número ({phone})")
+                        # raise e  # for debug
+
+    save_cache("sent_contact_names", set())
+    wpp.stop()
 
 
 if __name__ == "__main__":
